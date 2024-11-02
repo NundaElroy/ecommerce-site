67e61dba6d10088afbdbded1ef5c218095cfb9de
NundaElroy <sekyezaelroy@gmail.com>
2024-11-02 22:43:54 +0300

    unit and intergration  tests

---
+++ src/test/java/com/ecommerce/nunda/serviceImp/UserServiceImpTest.java
@@ -0,0 +1,19 @@
+package com.ecommerce.nunda.serviceImp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserServiceImpTest {
+
+    @Test
+    void getUserByEmail() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
