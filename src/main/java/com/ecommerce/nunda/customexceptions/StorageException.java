73eb43eea993b9e81e31312c779428da5e8ea9a8
NundaElroy <sekyezaelroy@gmail.com>
2024-12-17 00:30:14 +0300

    (new) exception for file storage

--- src/main/java/com/ecommerce/nunda/customexceptions/StorageException.java
+++ src/main/java/com/ecommerce/nunda/customexceptions/StorageException.java
@@ -1,2 +1,10 @@
-package com.ecommerce.nunda.customexceptions;public class StorageException {
+package com.ecommerce.nunda.customexceptions;
+
+public class StorageException extends  RuntimeException{
+    public  StorageException(String message){
+        super(message);
+    }
+    public  StorageException(String message ,Throwable cause){
+        super(message , cause);
+    }
 }
