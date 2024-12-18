f9cf94f6d1b0ab543a8eb821351360ba60a8caf7
NundaElroy <sekyezaelroy@gmail.com>
2024-12-18 13:52:25 +0300

    dropdown list for the given categpries

--- src/main/java/com/ecommerce/nunda/controller/UserController.java
+++ src/main/java/com/ecommerce/nunda/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.ecommerce.nunda.controller;
 
 
+import com.ecommerce.nunda.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    private final CategoryService categoryService;
+
+    public UserController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    //guest page for non auth users
     @GetMapping({"/guest",""})
-    public String index() {
+    public String index(Model model ) {
+        model.addAttribute("categoriesForDropdown", categoryService.getAllCategories());
         return "user/guest_user" ;
     }
 
